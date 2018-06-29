930c0397b43798a855b5394c825bbb7b74b71d09
pedropcruz <pedropcruz@outlook.pt>
2018-06-29 01:16:23 +0100

    test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose');
+const { mongoose } = require('./db/mongoose');
 
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.post('/todos', (req, res) => {
 		"text": req.body.text
 	});
 
-	todo.save().then((doc)=>{
+	todo.save().then((doc) => {
 		res.send(doc);
 	}, (e) => {
 		res.status(400).send(e);
@@ -25,3 +25,7 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
+
+module.exports = {
+	app
+};
